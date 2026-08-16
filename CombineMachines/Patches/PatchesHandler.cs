745cc95a0339e8c3a8a74fa4bf7c74d9744796e8
SrZek <zekonerap@gmail.com>
2026-08-16 20:16:18 +0200

    Register generic machine compatibility fallback

--- CombineMachines/Patches/PatchesHandler.cs
+++ CombineMachines/Patches/PatchesHandler.cs
@@ -69,6 +69,7 @@ namespace CombineMachines.Patches
 
             ProcessingPatches.Entry(helper, Harmony);
             CrystalariumRepeatCyclePatch.Entry(Harmony);
+            GenericMachineFallbackPatch.Entry(Harmony);
 
             //  Patch StardewValley.Object.minutesElapsed to detect when a machines output is ready for collecting, and when that happens, increase the output quantity
             //  to account for how many machines were combined
